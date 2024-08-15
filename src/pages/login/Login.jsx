b945389e53c0ed62199d7c97195d9104f2a23e6d
sadiqur057 <sadiqur057@gmail.com>
2024-08-15 15:28:17 +0600

    handled login functionality

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,33 @@
+import { useContext } from "react";
+import { useForm } from "react-hook-form"
+import { AuthContext } from "../../provider/AuthProvider";
+import { useNavigate, Link } from "react-router-dom"
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit
+  } = useForm()
+  const { signInUser, googleSignIn } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const onSubmit = (data) => {
+    console.log(data)
+    signInUser(data?.email, data?.password)
+      .then(result => {
+        console.log(result?.user)
+        navigate('/products')
+      })
+  }
+
+  const handleGoogleLogin = ()=>{
+    googleSignIn()
+    .then(result=>{
+      console.log(result)
+      navigate('/products')
+    })
+  }
+
   return (
     <div className="min-h-[calc(100vh-224px)] px-4 sm:px-10 py-3 bg-primary">
       <div className="w-full max-w-sm p-6 m-auto mx-auto  rounded-lg shadow-md ">
@@ -7,10 +35,10 @@ const Login = () => {
           <img className="w-auto h-7 sm:h-8" src="https://merakiui.com/images/logo.svg" alt="" />
         </div>
 
-        <form className="mt-6">
+        <form className="mt-6" onSubmit={handleSubmit(onSubmit)}>
           <div>
             <label htmlFor="email" className="block text-sm text-gray-800 dark:text-gray-200">Email</label>
-            <input type="email" name="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+            <input type="email" name="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" {...register("email", { required: true })} />
           </div>
 
           <div className="mt-4">
@@ -19,7 +47,7 @@ const Login = () => {
               <a href="#" className="text-xs text-gray-600 dark:text-gray-400 hover:underline">Forget Password?</a>
             </div>
 
-            <input type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+            <input type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" {...register('password', { required: true })} />
           </div>
 
           <div className="mt-6">
@@ -40,17 +68,12 @@ const Login = () => {
         </div>
 
         <div className="flex items-center mt-6 -mx-2">
-          <button type="button" className="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:bg-blue-400 focus:outline-none">
-            <svg className="w-4 h-4 mx-2 fill-current" viewBox="0 0 24 24">
-              <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z">
-              </path>
-            </svg>
-
-            <span className="hidden mx-2 sm:inline">Sign in with Google</span>
+          <button onClick={handleGoogleLogin} type="button" className="flex items-center justify-center w-full px-6 py-2.5 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:bg-blue-400 focus:outline-none">
+            Sign in with Google
           </button>
         </div>
 
-        <p className="mt-8 text-xs font-light text-center text-gray-400"> Don&apos;t have an account? <a href="#" className="font-medium text-gray-700 dark:text-gray-200 hover:underline">Create One</a></p>
+        <p className="mt-8 text-xs font-light text-center text-gray-400"> Don&apos;t have an account? <Link to="/register" className="font-medium text-gray-700 dark:text-gray-200 hover:underline">Create One</Link></p>
       </div>
     </div>
   );
