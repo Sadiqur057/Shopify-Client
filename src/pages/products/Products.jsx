e5f9ed17a171360920fd55ee9b29ad38d1704c81
sadiqur057 <sadiqur057@gmail.com>
2024-08-15 15:28:50 +0600

    modified spacing between cards

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
 import ProductCard from "./ProductCard";
+import { AuthContext } from "../../provider/AuthProvider";
+
 
 const Products = () => {
   // Product Name, Product Image, category  Description, Price, Category, Ratings and Product Creation date and time.
+  const {user} = useContext(AuthContext);
+
+  console.log(user)
   return (
     <div className="min-h-[calc(100vh-224px)] px-4 sm:px-10 py-4 sm:py-10 bg-primary">
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
