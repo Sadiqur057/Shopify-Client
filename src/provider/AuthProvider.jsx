f6d026dc138f8c7903893a02c53a2d72c637851e
sadiqur057 <sadiqur057@gmail.com>
2024-08-15 12:42:48 +0600

    created context with auth provider

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,54 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, signOut, signInWithPopup } from "firebase/auth";
+import auth from "../firebase/firebase.config";
+import { createContext, useEffect, useState } from "react";
+import PropTypes from 'prop-types'
+
+
+export const AuthContext = createContext(null)
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const googleProvider = new GoogleAuthProvider()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser)
+      if (currentUser) {
+        setLoading(false)
+      } else {
+        console.log('no user found')
+      }
+    })
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
+  const googleSignIn = ()=>{
+    setLoading(true)
+    return signInWithPopup(auth,googleProvider)
+  }
+
+  const logOut = () => {
+    setLoading(true)
+    return signOut(auth)
+  }
+
+  const authInfo = { user, loading, createUser, signInUser, logOut, googleSignIn }
+  return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+}
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export default AuthProvider;
