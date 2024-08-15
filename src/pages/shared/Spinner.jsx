c9d41255dbab0aa65de5f9f8f3190226a0aac7bd
sadiqur057 <sadiqur057@gmail.com>
2024-08-15 15:29:19 +0600

    created custom spinner

---
+++ src/pages/shared/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="min-h-[calc(100vh-224px)] flex justify-center items-center py-3 bg-primary">
+      <p className="text-white">Loading...</p>
+    </div>
+  );
+};
+
+export default Spinner;
