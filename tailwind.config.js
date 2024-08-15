079ace27ca08408ef79cff5fff8580c8c0b65efe
sadiqur057 <sadiqur057@gmail.com>
2024-08-15 15:30:53 +0600

    project setup installations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary':'#0f172a',
+        'secondary': '#1e293b',
+        'text': '#706f6f',
+      }
+    },
   },
   plugins: [],
 }
