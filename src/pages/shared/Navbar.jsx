4b5b2fbec6a2819c618974fa02d6b772154b9956
sadiqur057 <sadiqur057@gmail.com>
2024-08-15 15:29:36 +0600

    handled functionality of logout

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,8 +1,31 @@
+import { useContext } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
+  const { user, loading, logOut } = useContext(AuthContext);
+
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logOut()
+      .then(()=>{
+        navigate('/login')
+      })
+      .catch(error => console.log(error))
+  }
+
   return (
     <div className="bg-secondary h-16 flex px-4 sm:px-10 justify-between items-center">
       <h2 className="text-white font-semibold text-xl">ShopiFy</h2>
-      <button className="bg-primary text-gray-100 px-5 py-2 rounded-3xl">Login</button>
+      {
+        loading && <p className="text-gray-300">Loading..</p>
+      }
+      {
+        user && <button onClick={handleLogout} className="bg-primary text-gray-100 px-5 py-2 rounded-3xl">Logout</button>
+      }
+      {
+        !user && !loading && <button className="bg-primary text-gray-100 px-5 py-2 rounded-3xl">Login</button>
+      }
     </div>
   );
 };
