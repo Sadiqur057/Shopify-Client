cf7a7d016aa996e0b96f3076241ff3ad5e8cdacf
sadiqur057 <sadiqur057@gmail.com>
2024-08-15 15:29:04 +0600

    handled account creation functionality

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,60 @@
+import { useContext } from "react";
+import { useForm } from "react-hook-form"
+import { AuthContext } from "../../provider/AuthProvider";
+import { Link, useNavigate } from 'react-router-dom'
+
 const Register = () => {
+  const navigate = useNavigate()
+  const {
+    register,
+    handleSubmit
+  } = useForm()
+  const {createUser} = useContext(AuthContext)
+
+  const onSubmit = (data) => {
+    console.log(data)
+    createUser(data?.email, data?.password)
+    .then(result=>{
+      console.log(result?.user)
+      navigate('/products')
+    })
+    
+  }
+
   return (
-    <div>
-      reg
+    <div className="min-h-[calc(100vh-224px)] px-4 sm:px-10 py-3 bg-primary">
+      <div className="w-full max-w-sm p-6 m-auto mx-auto  rounded-lg shadow-md ">
+        <div className="flex justify-center mx-auto">
+          <img className="w-auto h-7 sm:h-8" src="https://merakiui.com/images/logo.svg" alt="" />
+        </div>
+
+        <form className="mt-6" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label htmlFor="name" className="block text-sm text-gray-800 dark:text-gray-200">Name</label>
+            <input type="text" name="name" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 mb-3" {...register("name", { required: true })} />
+          </div>
+          <div>
+            <label htmlFor="email" className="block text-sm text-gray-800 dark:text-gray-200">Email</label>
+            <input type="email" name="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" {...register("email", { required: true })} />
+          </div>
+
+          <div className="mt-4">
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm text-gray-800 dark:text-gray-200">Password</label>
+            </div>
+
+            <input type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" {...register('password', {required:true})}/>
+          </div>
+
+          <div className="mt-6">
+            <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+              Register
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-8 text-xs font-light text-center text-gray-400"> Already have an account? <Link to='/login' className="font-medium text-gray-700 dark:text-gray-200 hover:underline">Login here</Link></p>
+      </div>
     </div>
   );
 };
