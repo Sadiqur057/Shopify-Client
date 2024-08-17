82a43c12af6f4a2bc2e880431aeeea37b0571df1
sadiqur057 <sadiqur057@gmail.com>
2024-08-17 15:55:43 +0600

    updated input styling

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -12,10 +12,8 @@ const Login = () => {
   const navigate = useNavigate()
 
   const onSubmit = (data) => {
-    console.log(data)
     signInUser(data?.email, data?.password)
-      .then(result => {
-        console.log(result?.user)
+      .then(() => {
         navigate('/products')
       })
   }
@@ -31,27 +29,24 @@ const Login = () => {
   return (
     <div className="min-h-[calc(100vh-224px)] px-4 sm:px-10 py-3 bg-primary">
       <div className="w-full max-w-sm p-6 m-auto mx-auto  rounded-lg shadow-md ">
-        <div className="flex justify-center mx-auto">
-          <img className="w-auto h-7 sm:h-8" src="https://merakiui.com/images/logo.svg" alt="" />
-        </div>
 
         <form className="mt-6" onSubmit={handleSubmit(onSubmit)}>
           <div>
-            <label htmlFor="email" className="block text-sm text-gray-800 dark:text-gray-200">Email</label>
-            <input type="email" name="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" {...register("email", { required: true })} />
+            <label htmlFor="email" className="block text-sm text-gray-200">Email</label>
+            <input type="email" name="email" className="block w-full px-4 py-2 mt-2 text-gray-400 border border-gray-700 rounded-lg bg-secondary focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" {...register("email", { required: true })} />
           </div>
 
           <div className="mt-4">
             <div className="flex items-center justify-between">
-              <label htmlFor="password" className="block text-sm text-gray-800 dark:text-gray-200">Password</label>
-              <a href="#" className="text-xs text-gray-600 dark:text-gray-400 hover:underline">Forget Password?</a>
+              <label htmlFor="password" className="block text-sm text-gray-200">Password</label>
+              <a href="#" className="text-xs text-gray-400 hover:underline">Forget Password?</a>
             </div>
 
-            <input type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" {...register('password', { required: true })} />
+            <input type="password" className="block w-full px-4 py-2 mt-2 text-gray-400 border border-gray-700 rounded-lg bg-secondary focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" {...register('password', { required: true })} />
           </div>
 
           <div className="mt-6">
-            <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+            <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-secondary rounded-lg hover:bg-blue-gray-900 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
               Sign In
             </button>
           </div>
@@ -73,7 +68,7 @@ const Login = () => {
           </button>
         </div>
 
-        <p className="mt-8 text-xs font-light text-center text-gray-400"> Don&apos;t have an account? <Link to="/register" className="font-medium text-gray-700 dark:text-gray-200 hover:underline">Create One</Link></p>
+        <p className="mt-8 text-xs font-light text-center text-gray-400"> Don&apos;t have an account? <Link to="/register" className="font-medium text-gray-200 hover:underline">Create One</Link></p>
       </div>
     </div>
   );
