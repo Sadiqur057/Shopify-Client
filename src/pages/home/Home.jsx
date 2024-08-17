41b0801c36ae42f3a25923d2b6eadadfafa8b1bd
sadiqur057 <sadiqur057@gmail.com>
2024-08-17 15:55:28 +0600

    updated button text

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,8 @@ const Home = () => {
   return (
     <div className="min-h-[calc(100vh-224px)] px-4 sm:px-10 py-3 bg-primary flex flex-col items-center justify-center gap-3">
       <h1 className="text-center text-white font-bold text-3xl">Find Best Products Here</h1>
-      <h1 className="text-center text-gray-400 max-w-[560px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis aliquid rerum tempore debitis nihil laborum vel nesciunt eaque ipsam veniam corrupti nisi eligendi ab id numquam porro, eius, minima culpa.</h1>
-      <Link to='/products'> <button className="bg-secondary text-gray-100 px-6 py-3 rounded-3xl hover:bg-[#294064]">Get Now</button></Link>
+      <h1 className="text-center text-gray-400 max-w-[560px]">Discover top-quality Electronics, Fashion, and Home & Office essentials at ShopiFy! Explore our wide range of products from trusted brands, designed to elevate your lifestyle. Shop now and enjoy unbeatable deals on the latest must-have items!</h1>
+      <Link to='/products'> <button className="bg-secondary text-gray-100 px-6 py-3 rounded-3xl hover:bg-[#294064]">View Products</button></Link>
     </div>
   );
 };
