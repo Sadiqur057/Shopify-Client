bf09367f81d723954e66e4d621dffb95202ba87d
sadiqur057 <sadiqur057@gmail.com>
2024-09-13 20:39:13 +0600

    customized home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,32 @@
 import { Link } from 'react-router-dom'
 const Home = () => {
   return (
-    <div className="min-h-[calc(100vh-224px)] px-4 sm:px-10 py-3 bg-primary flex flex-col items-center justify-center gap-3">
-      <h1 className="text-center text-white font-bold text-3xl">Find Best Products Here</h1>
-      <h1 className="text-center text-gray-400 max-w-[560px]">Discover top-quality Electronics, Fashion, and Home & Office essentials at ShopiFy! Explore our wide range of products from trusted brands, designed to elevate your lifestyle. Shop now and enjoy unbeatable deals on the latest must-have items!</h1>
-      <Link to='/products'> <button className="bg-secondary text-gray-100 px-6 py-3 rounded-3xl hover:bg-[#294064]">View Products</button></Link>
-    </div>
+    <div className="md:container px-6 py-16 mx-auto">
+          <div className="items-center lg:flex">
+            <div className="w-full lg:w-1/2">
+              <div className="lg:max-w-lg">
+                <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">
+                  Best place to choose <br /> your {" "}
+                  <span className="text-blue-500">Products</span>
+                </h1>
+
+                <p className="my-3 text-gray-600 dark:text-gray-400">
+                Discover top-quality Electronics, Fashion, and Home & Office essentials at ShopiFy! Explore our wide range of products from trusted brands, designed to elevate your lifestyle. Shop now and enjoy unbeatable deals on the latest must-have items!
+                </p>
+
+                <Link to='/products'> <button className="bg-blue-600 hover:bg-blue-500 text-gray-100 px-6 py-3 rounded-3xl">View All Products</button></Link>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+              <img
+                className="w-full h-full lg:max-w-3xl"
+                src="https://merakiui.com/images/components/Catalogue-pana.svg"
+                alt="Catalogue-pana.svg"
+              />
+            </div>
+          </div>
+        </div>
   );
 };
 
